65997b68359a6c29acec31078d82403ec5b6f7f8
LeoLetsPlay3rd <113899208+LeoLetsPlay3rd@users.noreply.github.com>
2022-11-24 16:23:31 +0100

    MatchStats

--- Assets/Scripts/MatchStats.cs
+++ Assets/Scripts/MatchStats.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 
 public class MatchStats : ScriptableObject
 {
+    public static MatchStats instance;
+
     public int player1;
     public int player2;
+
+    private int player1start = 0;
+    private int player2start = 0;
+
+    private void OnEnable()
+    {
+        instance = this;
+        player1 = player1start;
+        player2 = player2start;
+    }
 
     public void AddPointPlayer1()
     {
